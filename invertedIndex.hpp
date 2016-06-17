41ac9e4244ac6efdbebb1b73ab5c4e015a1afd0f
cheshirecatnick@gmail.com <cheshirecatnick@gmail.com>
2016-06-17 11:55:00 +0800

    finish inverted index

--- invertedIndex.hpp
+++ invertedIndex.hpp
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 #include <map>
 #include <vector>
@@ -8,35 +9,40 @@
 #define MAXFILENAME 1000
 #define MAXWORDNUM 30000
 #define MAXWORDLEN 20
+#define MAXFILENUM 30000
+#define MAXTERMNUM 1100000 // this should be checked
+
+using namespace std;
 
 char fileListFilename[MAXFILENAME];
 char invFilename[MAXFILENAME];
 char vocabFilename[MAXFILENAME];
+// string compare for inverts
+struct cmp_str{
+	bool operator()(char* a, char* b){
+		return strcmp(a, b) < 0;
+	}
+};
+char words[MAXWORDNUM][MAXWORDLEN];			// wordID => word string
+int maxFreq[MAXFILENUM];					// fileID => maxFreq
+map<int, int> wordCounts[MAXTERMNUM];		// fileID => wordCount
+char terms[MAXTERMNUM][2 * MAXWORDLEN + 1];	// termID => term string
+map<char*, map<int, int>*, cmp_str> inverts;			// term string => wordCounts map
 
-char words[MAXWORDNUM][MAXWORDLEN];
+
+
 /*
 #define MAXTERNUM 10
-#define MAXTERMNUM 1100000 //1065981?
-#define MAXFILENUM 46973
-using namespace std;
 
 typedef struct data{
 	int fileNum;
 	map<int, int> wordCount; //fileID -> count
 } Data;
 
-struct cmp_str{
-	bool operator()(char* a, char* b){
-		return strcmp(a, b) < 0;
-	}
-};
 
-Data datum[MAXTERMNUM];
-char terms[MAXTERMNUM][7];
-map<char*, Data*, cmp_str> inverts;
+
 map<char*, Data*>::iterator it;
 char files[MAXFILENUM][MAXFILENAME]; //fileID -> fileName
-int maxFreq[MAXFILENUM];	//fileID -> maxFreq
 int fileNum;
 char modelDir[1000];
 
@@ -56,23 +62,21 @@ void readVocab(){
 	while (fscanf(f, "%s", words[wordNum]) != EOF)
 		wordNum++;
 }
-/*
+
 void createInverts(){
-	char invertedFile[1000];
-	strncpy(invertedFile, modelDir, 1000);
-	strncat(invertedFile, "/inverted-file", 30);
-	FILE* f = fopen(invertedFile, "r");
+	FILE* f = fopen(invFilename, "r");
 	int vID1, vID2, fileNum, fileID, count;
 	pair<int, int> p;
-	pair<char*, Data*> item;
+	pair<char*, map<int, int>* > item;
 	int termNum = 0;
-
+	// init maxFreq
 	for (int i = 0; i < MAXFILENUM; i++)
 		maxFreq[i] = 0;
-	
+	// create inverted index
 	while (fscanf(f, "%d %d %d", &vID1, &vID2, &fileNum) != EOF){
 		//printf("%d, %d, %d\n", vID1, vID2, fileNum);
-		//No English and number
+		/*
+		// No English or number
 		if (vID1 < 5391 || vID1 > 12276 || vID2 < 5391 || vID2 > 12276){
 			for (int i = 0; i < fileNum; i++){
 				fscanf(f, "%d %d", &fileID, &count);
@@ -80,6 +84,7 @@ void createInverts(){
 			}
 			continue;
 		}
+		// eat data with English or number
 		if (isalpha(words[vID1][0]) || isdigit(words[vID1][0])
 			|| isalpha(words[vID2][0]) || isdigit(words[vID2][0])){
 			for (int i = 0; i < fileNum; i++){
@@ -87,32 +92,38 @@ void createInverts(){
 				//printf("fileID = %d, count = %d\n", fileID, count);
 			}
 			continue;
-		}
+		}*/
 
 		//printf("%d, %d, %d\n", vID1, vID2, fileNum);
-		datum[termNum].fileNum = fileNum;
+		// create wordCounts
 		for (int i = 0; i < fileNum; i++){
 			fscanf(f, "%d %d", &fileID, &count);
 			//printf("fileID = %d, count = %d\n", fileID, count);
 			maxFreq[fileID] = count > maxFreq[fileID] ? count : maxFreq[fileID];
 			p = make_pair(fileID, count);
-			datum[termNum].wordCount.insert(p);
+			wordCounts[termNum].insert(p);
 		}
-		
-		strncpy(terms[termNum], words[vID1], 3);
+		// create terms
+		strncpy(terms[termNum], words[vID1], strlen(words[vID1]));
+		// TODO: vID2 is English?
 		if (vID2 != -1)
-			strncat(terms[termNum], words[vID2], 3);
-		terms[termNum][6] = '\0';
+			strncat(terms[termNum], words[vID2], strlen(words[vID2]));
 
-		item = make_pair(terms[termNum], &datum[termNum]);
+		//printf("%s\n", terms[termNum]);
+		item = make_pair(terms[termNum], &wordCounts[termNum]);
 		inverts.insert(item);
 		//printf("%s\n", item.first);
 		termNum++;
 	}
-	//for (it = inverts.begin(); it != inverts.end(); it++)
-	//	printf("%s\n", it->first);
+	/*
+	map<char*, map<int, int>*, cmp_str>::iterator it;
+	for (it = inverts.begin(); it != inverts.end(); it++)
+		printf("%s\n", it->first);
+	printf("%d\n", inverts.size());
+	*/
 }
 
+/*
 void createFileName(){
 	char fileList[1000];
 	strncpy(fileList, modelDir, 1000);

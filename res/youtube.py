f111b57335fb66002fb368c4a6ba16ea107fb8c8
DearLinny <b02902045@ntu.edu.tw>
2016-06-18 23:53:52 +0800

    read query and result,find url with time, launch browser

--- res/youtube.py
+++ res/youtube.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import webbrowser
 
 from apiclient.discovery import build
 from apiclient.errors import HttpError
@@ -13,27 +14,34 @@ DEVELOPER_KEY = "AIzaSyAAWsdajiwk9v84_ZctzmFbqnmXADVTBxM"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def time_search(keyword_path, lrc_path, videoid):
+def time_search(keyword_path, videoid, song_path):
   keyword_list=[]
   key_count=0
   with open(keyword_path,'r', encoding='big5') as keyword_f:
     for word in keyword_f:
+      word=word.rstrip('\n')
       keyword_list.append(word)
       key_count = key_count+1
-      
-  with open(lrc_path,'r', encoding='big5') as lrc_f:
+  #print ("%s" % keyword_list)
+  with open(song_path,'r', encoding='big5') as song_f:
     find_word=0
-    time_list=[]
-    for line in lrc_f:
+    for line in song_f:
       #print("%s" % line)
-      for i in range(len(keyword_list)):
+      i=0
+      while i < key_count:
         if keyword_list[i] in line:
+          j=0
           for j in range(len(line)):
-            j=line.find("[")
-          print("%d" % j)
-          print ("https://www.youtube.com/watch?v=%s&feature=youtu.be&t=%sm%ss\n" % (videoid, line[2], line[4:6]));
+            if line.find("[", j) == -1:
+              break
+          sec=int(line[j+3:j+5])
+          final_url=videoid+"&feature=youtu.be&t="+line[j+1]+"m"+str(sec)+"s"
+          print ("%s" % final_url);
+          webbrowser.open(final_url)
           find_word=1
           break
+        i=i+1
+
       if find_word==1:
         break
 
@@ -63,17 +71,20 @@ def youtube_search(options):
     if search_result["id"]["kind"] == "youtube#video":
       #videos.append("%s (%s)" % (search_result["snippet"]["title"],search_result["id"]["videoId"]))  
       title=search_result["snippet"]["title"].lower()
-      #print ("%s %s" % (title, options.q))
+      #if options.q[0] in title and options.q[1] in title:
+      #  best_url=search_result["id"]["videoId"]
+      #print ("%s" %title)
       if options.q[0] not in title or (options.q[1] not in title):
         tmp_result=3
-      else:
-        if 'MV' in title:
-          tmp_result=1
-        elif 'music video' in title:
-          tmp_result=1
-        else:
-          tmp_result=0
-
+      else:        
+        #if 'mv' in title:
+        #  tmp_result=1
+        #elif 'music video' in title:
+        #  tmp_result=1
+        #else:
+       
+        tmp_result=0
+     
       if tmp_result < best_result:
         best_url=search_result["id"]["videoId"]
         best_result=tmp_result
@@ -81,15 +92,12 @@ def youtube_search(options):
     #find result with singer and song name, no mv...
       if tmp_result==0:
         break
-    #print ("https://www.youtube.com/watch?v=%s %d" % (best_url, best_result))
+      
   return best_url  
-  #print ("Videos:\n", "\n".join(videos), "\n")
-
 
 if __name__ == "__main__":
   f = open('search.txt','r')
   keyword_path='keyword.txt'
-  lrc_path='Ask  For  More.lrc'
 
   #set argparse
   argparser.add_argument("--q", help="Search term", default='')
@@ -101,14 +109,14 @@ if __name__ == "__main__":
   count=0
   while find_song==0 and count<10:
     search_var=f.readline()
+    song_path=search_var.rstrip('\n')
     #get singer and song name
     #search_var is a list, search[0]=singer, search_var[1]=song name
-    search_var=search_var.rstrip('\n')
+    search_var=search_var.rstrip('.lrc\n')
     search_var=search_var.split('/')
     if len(search_var) > 2:
       length=len(search_var)
       search_var=search_var[length-2:length]
-    #print ("%s" % search_var)
 
     #search by keyword
     args.q=search_var
@@ -117,7 +125,6 @@ if __name__ == "__main__":
       #check if video is found
       if videoid != "not found":
         videoid='https://www.youtube.com/watch?v='+videoid 
-        print("%s" % videoid)
         find_song=1
         break
 
@@ -125,7 +132,12 @@ if __name__ == "__main__":
       print ("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
  
   #get lrc time and combine url with time
-  #if find_song==1:
-  #  time_search(keyword_path, lrc_path, videoid)
-  #else:
-   # print("not found")
+  if find_song==1:
+  
+    #modify path for windows    
+    #song_path=song_path.replace('/', '\\')
+ 
+    time_search(keyword_path, videoid, "lrcc"+song_path)
+
+  else:
+    print("not found")
